2448ee19e11f92a3d47b30604f0d453661884be6
pankaj <pankaj_awaghad@persistent.co.in>
2018-11-15 16:01:51 +0530

    get_tag.sh modified

--- get_tag.sh
+++ get_tag.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
-tag=`git describe --tag`
-echo $tag >> temp.txt
+repo="Hello-Jenkins HelloWorldJava"
+echo $repo
+for i in $repo
+	do
+		echo "Cloning the $i repo"
+		git clone https://github.com/pankajja/$i.git
+		echo "The latest tag for $i is" >> temp.txt
+		cd $i 
+		git describe --tags --abbrev=0 >> ../temp.txt
+		cd ../
+		rm -rf $i
+	done
