f47f401022d08ab7ebc1336880512844e726e64b
pankaj <pankaj_awaghad@persistent.co.in>
2018-11-19 14:44:57 +0530

    file reverted back

--- get_tag.sh
+++ get_tag.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
-#repo="Hello-Jenkins HelloWorldJava"
-echo ${bamboo.repo}
-for i in ${bamboo.repo}
-	do
-		echo "Cloning the $i repo"
-		git clone https://github.com/pankajja/$i.git
-		echo "The latest tag for repo $i is" >> temp.txt
-		cd $i 
-		git describe --tags --abbrev=0 >> ../temp.txt
-		cd ../
-		rm -rf $i
-	done
+repo="Hello-Jenkins HelloWorldJava"
+echo $repo
+	for i in $repo
+		do
+			echo "Cloning the $i repo"
+			git clone https://github.com/pankajja/$i.git
+			echo "The latest tag for repo $i is" >> temp.txt
+			cd $i 
+			git describe --tags --abbrev=0 >> ../temp.txt
+			cd ../
+			rm -rf $i
+		done
+fi
