62f79ac1cfe31b6e5fc85b9643b65cc3961ff462
pankaj <pankaj_awaghad@persistent.co.in>
2018-11-29 13:11:13 +0530

    modified script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@
 #export version="${bamboo.version}"
 echo $dt
 echo $version
-sed -e "s/^Build .*$/Build date : $dt/" ./version/version.txt
-sed -e "s/^Version .*$/Version : $version/" ./version/version.txt
+sed -i -e "s/^Build .*$/Build date : $dt/" ./version/version.txt
+sed -i -e "s/^Version .*$/Version : $version/" ./version/version.txt
