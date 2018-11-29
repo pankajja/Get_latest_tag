cd9fb63c8a3e6962573951ffb41b12201ebb45b8
pankaj <pankaj_awaghad@persistent.co.in>
2018-11-29 14:12:44 +0530

    modified script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/bash
 #export dt="${bamboo.dt}"
 #export version="${bamboo.version}"
-echo $dt
-echo $version
-sed -i -e "s/^Build .*$/Build date : $dt/" ./version/version.txt
-sed -i -e "s/^Version .*$/Version : $version/" ./version/version.txt
+#echo $dt
+#echo $version
+if [[ -z "$dt" ]] && [[ -z "$version" ]] ; then
+	echo "string is empty , nothing to change"
+else
+	if [[ -z "$version_name" ]]; then
+		echo "string is empty , nothing to change"
+	else
+		sed -i -e "s/^Build .*$/Build date : $dt/" ./version/version.txt
+		sed -i -e "s/^Version .*$/Version : $version/" ./version/version.txt
+		echo "version Name :" $version_name\_$version>> ./version/version.txt
+	fi
+fi 
